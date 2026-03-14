40b49c17e914cef9221103b64bef938a75664b9f
Ujjwal Bharti <ujjwalbharti@Ujjwals-MacBook-Air.local>
2026-03-14 20:14:29 +0530

    logged token

--- middleWare/authmiddleware.js
+++ middleWare/authmiddleware.js
@@ -16,6 +16,7 @@ const authMiddleware = (req, res, next) => {
   }
 
   const token = req.headers.authorization?.split(' ')[1];
+  console.log("Extracted Token:", token);
   
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
